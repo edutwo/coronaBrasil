5d066ab83bcbb009768e2c10a1f9474426dd10f9
edutwo <61384148+edutwo@users.noreply.github.com>
2020-03-22 21:17:16 -0300

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,24 +2,12 @@ const mySql = require('mysql2')
 
 const connCoro = mySql.createPool ({
     connectionLimit : 30,
-    host            : '192.168.254.250',
+    host            : '192.168.254.xx',
     database        : 'coronaVirus',
-    user            : 'root',
-    password        : 'santoedu',
+    user            : 'user',
+    password        : 'senha',
     charset         : `UTF8MB4_GENERAL_CI`
 })
 
-const connTel = mySql.createPool ({
-    connectionLimit : 30,
-    host            : '192.168.254.250',
-    database        : 'baseTelegram',
-    user            : 'root',
-    password        : 'santoedu',
-    charset         : `UTF8MB4_GENERAL_CI`
-})
-
-
-
-
-module.exports = {connCoro, connTel}
+module.exports = {connCoro}
 
